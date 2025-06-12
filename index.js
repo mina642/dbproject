9b3c38c3ce001b17ed992e57b893adf220bac44b
mina642 <minarana628@gmail.com>
2025-06-12 16:40:11 +0545

    connect to database

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const { Client } = require('pg');
+
 const app = express();
 const PORT = 3000;
 
+// PostgreSQL connection config
+const dbClient = new Client({
+  user: 'postgres', // change if your username is different
+  host: 'localhost',
+  database: 'postgres', // change to your database name if needed
+  password: 'postgres', // change to your password
+  port: 5432,
+});
+
+dbClient.connect()
+  .then(() => console.log('Connected to PostgreSQL'))
+  .catch(err => console.error('PostgreSQL connection error:', err));
+
 app.get('/', (req, res) => {
-  res.send('hello word');
+  res.send('hello word from Mina');
 });
 
 app.listen(PORT, () => {
